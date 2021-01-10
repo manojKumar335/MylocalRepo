18b900af30d27da0b89061823a5c2504225ca4f0
Manoj <manojkumar.kashyap74@gmail.com>
2021-01-10 22:52:05 +0530

    My first commit

---
+++ HomeController.java
@@ -0,0 +1,17 @@
+package com.javainuse.springbootsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+   
+	@RequestMapping("/home")
+	public String home() {
+		
+		System.out.println("HomeController.home()");
+		return "home";
+	}
+	
+	
+}
